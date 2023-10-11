78dbaeff027ee5ee1da7f2718d46f967f21e1054
ChrisRyanTang <chrisryantang@gmail.com>
2023-10-11 11:27:22 -0700

    Initialize repo

---
+++ starter 6 2/IOhandler.js
@@ -0,0 +1,48 @@
+/*
+ * Project: Milestone 1
+ * File Name: IOhandler.js
+ * Description: Collection of functions for files input/output related operations
+ *
+ * Created Date:
+ * Author:
+ *
+ */
+
+/**
+ * Description: decompress file from given pathIn, write to given pathOut
+ *
+ * @param {string} pathIn
+ * @param {string} pathOut
+ * @return {promise}
+ */
+const unzip = (pathIn, pathOut) => {};
+
+/**
+ * Description: read all the png files from given directory and return Promise containing array of each png file path
+ *
+ * @param {string} path
+ * @return {promise}
+ */
+const readDir = (dir) => {};
+
+/**
+ * Description: Read in png file by given pathIn,
+ * convert to grayscale and write to given pathOut
+ *
+ * @param {string} filePath
+ * @param {string} pathProcessed
+ * @return {promise}
+ */
+
+const unzipper = require("unzipper");
+const fs = require("fs");
+const PNG = require("pngjs").PNG;
+const path = require("path");
+
+const grayScale = (pathIn, pathOut) => {};
+
+module.exports = {
+  unzip,
+  readDir,
+  grayScale,
+};
